21c14143d09d9438df937a3f5473288e478fa521
farzin <farzink25@gmail.com>
2023-08-06 23:13:28 +0330

    pathname

--- src/components/productHeader/index.tsx
+++ src/components/productHeader/index.tsx
@@ -7,22 +7,21 @@ interface PropTypes{
   data?:any
 }
 const ProductTitle:FC<PropTypes> = ({data}) => {
-  const location:any=typeof window!=='undefined'? window.location.pathname:'/'
+  const location:any= window.location.pathname?.startsWith('/product')
   const [heart, setheart] = useState(false)
   const [bell, setbell] = useState(false)
   const router=useRouter()
   return (
     <StAll>
        <div className="back">{location==='/product'?<AiOutlineArrowLeft onClick={()=>router.back()}/>:" "}</div>
-       <h2>{location!=='/product'?"products":data?.name1}</h2>
-       {location!=='/product'&&<span>{" "}</span>}
-       {location==='/product'&&<div className='btns'>{bell?<AiFillBell onClick={()=>setbell(!bell)} style={{fill:'rgb(215, 57, 72)'}}/>:<AiOutlineBell  
+       <h2>{!location?"products":data?.name1}</h2>
+       {!location&&<span>{" "}</span>}
+       {location&&<div className='btns'>{bell?<AiFillBell onClick={()=>setbell(!bell)} style={{fill:'rgb(215, 57, 72)'}}/>:<AiOutlineBell  
        
         onClick={()=>setbell(!bell)}/>}
         {heart?<AiFillHeart style={{fill:'rgb(215, 57, 72)'}}onClick={(e)=>{
           e.preventDefault()
           setheart(!heart)}}
-
         />:<AiOutlineHeart 
          onClick={(e)=>{
           e.preventDefault()
