cf1e413c84127d13362bae51e04095784c3d2b89
farzin <farzink25@gmail.com>
2023-08-06 19:05:09 +0330

    re design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,8 @@ import dynamic from 'next/dynamic'
 import Sidebar from '@/components/sidebar'
 import { Image, Row } from 'antd'
 import FullFooter from '@/components/fullFooter'
-// const HomeNavbar = dynamic(() => import('../components/home/homeNavbar'), { ssr: false })
-// const SearchNavbar = dynamic(() => import('../components/searchNavbar'), { ssr: false })
+const HomeNavbar = dynamic(() => import('../components/home/homeNavbar'), { ssr: false })
+const SearchNavbar = dynamic(() => import('../components/searchNavbar'), { ssr: false })
 
 const NotFound = () => {
   useEffect(() => {
@@ -14,10 +14,10 @@ const NotFound = () => {
   
   return (
     <div>
-    {/* <SearchNavbar/> */}
+    <SearchNavbar/>
     <br />
-    {/* <HomeNavbar /> */}
-    <Row>
+    <HomeNavbar />
+    <Row justify='center'>
       
    <Row  justify='center'>
     <img style={{width:600,height:600}} src="https://torob.com/static/images/error_404.jpg"/>
